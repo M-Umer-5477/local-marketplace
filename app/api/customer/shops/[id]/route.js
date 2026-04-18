c4bc993317c46a82b5508a4608642c0af7387baa
Muhammad Umer <muhammadumer5477@gmail.com>
2026-04-18 19:21:10 +0500

    fixed shop/[id] route error

--- app/api/customer/shops/[id]/route.js
+++ app/api/customer/shops/[id]/route.js
@@ -3,11 +3,12 @@ import db from "@/lib/db";
 import Seller from "@/models/seller";
 import Product from "@/models/product"; // Assuming you have this model
 import mongoose from "mongoose";
-
+import Order from "@/models/order";
+import User from "@/models/user";
 export async function GET(req, { params }) {
   try {
     await db.connect();
-    const { id } = await params;
+    const { id } = params;
 
     if (!mongoose.isValidObjectId(id)) {
       return NextResponse.json({ error: "Invalid shop ID" }, { status: 400 });
@@ -30,10 +31,9 @@ export async function GET(req, { params }) {
     // 4. Fetch Recent Reviews
     // We only need a few latest reviews to show on the shop page.
     // Instead of importing the whole Order model at the top if it's already there or importing it now.
-    const OrderModel = mongoose.models.Order || mongoose.model("Order");
-    const User = mongoose.models.User || mongoose.model("User");
     
-    const recentReviews = await OrderModel.find({ shopId: id, isReviewed: true })
+    
+    const recentReviews = await Order.find({ shopId: id, isReviewed: true })
       .select("rating feedback userId createdAt")
       .populate({ path: "userId", select: "name", model: User })
       .sort({ createdAt: -1 })
